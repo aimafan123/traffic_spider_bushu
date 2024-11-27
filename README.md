d420dd81fd857c5bba4eddc417c12239996c2a85
ZGC-BUPT-aimafan <chongrufan@nuaa.edu.cn>
2024-11-27 11:34:13 +0800

    v1.0

---
+++ README.md
@@ -0,0 +1,57 @@
+# 自动部署流量采集程序
+
+可以实现流量采集程序的自动部署，可以一次性在服务器上开启多个docker采集流量
+
+docker代码项目路径 [https://github.com/ZGC-BUPT-aimafan/spider_traffic.git]()
+
+## 部署步骤
+
+**注意1：需事先在服务器上安装docker并配置公私钥**
+
+**注意2：如果是非root用户登录服务器，需要手动在服务器上执行`ethtool -K docker0 tso off gso off gro off`**
+
+1. 安装依赖库
+```
+pip install -r requirments.txt
+```
+2. 将xray客户端配置文件放到`data/xray_config/`目录中
+3. 将需要采集的网站写入到项目目录的`urls.txt`文件中
+4. 将docker镜像文件放在`data/spider_traffic.tar`，以便等会儿上传到服务器
+5. 修改`config/config.ini`，将镜像名称修改为当前使用的镜像名称
+6. 将服务器信息填到`src/traffic_spider_bushu/server_info.py.example`，并将文件名称改为`server_info.py`
+7. 在`src`目录，执行
+```
+python -m traffic_spider_bushu.action
+```
+程序自动执行，部署结束
+
+
+## 服务器部署指令
+
+部署过程将在服务器上依次执行以下指令，如果不需要某些指令，可手动在代码中注释
+
+```bash
+scp -r data/spider_traffic.tar {username}@{host_name}:~/
+scp -r data/xray_config/{xray_name} {username}@{host_name}:~/
+
+ethtool -K docker0 tso off gso off gro off  #如果非root用户，该指令不会成功
+
+docker load -i {remote_image_path}
+
+mkdir -p {storage_path}/{container_name}/config # container_name 定义为spider_traffic_i，i是docker序号，从0开始
+
+echo {config_content} > {config_dir}/config.ini # 写入配置文件，配置文件的内容在代码中定义
+echo {url_parts[i]} > {config_dir}/current_docker_url_list.txt  # 将要访问的url写入文件，该docker要访问的url为全部url的1/n份，其中n为开启的docker的数量
+echo {get_exclude_content()} > {config_dir}/exclude_keywords    # 将要排除的内容写入文件，排除的内容在代码中定义
+
+docker stop {container_name}
+docker rm {container_name}
+
+docker run -v {storage_path}/{container_name}/data:/app/data -v {config_dir}:/app/config -v {storage_path}/{container_name}/logs:/app/logs --privileged -itd --name {container_name} {image_name} /bin/bash
+
+docker exec  {container_name} ethtool -K eth0 tso off gso off gro off lro off   # docker内关闭合并包
+
+docker cp {remote_xray_path} {container_name}:/app/config
+
+nohup docker exec {container_name} bash action.sh > {storage_path}/{container_name+'.log'} 2>&1 &   # 后台执行采集指令，并输出日志
+```
---
+++ src/traffic_spider_bushu/action.py
@@ -0,0 +1,200 @@
+import os
+import time
+
+import paramiko
+from scp import SCPClient
+
+from traffic_spider_bushu.myutils import project_path
+from traffic_spider_bushu.myutils.config import config
+from traffic_spider_bushu.myutils.logger import logger
+from traffic_spider_bushu.server_info import servers_info
+
+index = 0
+
+
+# 异步执行并监控命令输出
+def exec_command(client, command):
+    # logger.info(f"执行{command}")
+    stdin, stdout, stderr = client.exec_command(command)
+
+    while not stdout.channel.exit_status_ready():
+        # 逐行读取输出
+        line = stdout.readline()
+        if line:
+            print(f"{line.strip()}")
+        time.sleep(1)  # 异步等待，避免阻塞
+
+    # 读取剩余的输出
+    err = stderr.read().decode()
+    if err:
+        print(f"{err}")
+
+
+def get_exclude_content():
+    exclude_str = """egister
+ignin
+ogin
+Help
+help
+policies
+Policies
+policy
+Policy
+"""
+    return exclude_str
+
+
+def get_running_content():
+    running_str = """{"currentIndex": 0}"""
+    return running_str
+
+
+def run_command(ssh, command):
+    logger.info(command)
+    stdin, stdout, stderr = ssh.exec_command(command)
+    logger.info(stdout.read().decode())
+    logger.info(stderr.read().decode())
+
+
+# 异步上传文件
+def async_upload_file(sftp, local_file, remote_file):
+    if os.path.exists(local_file):
+        sftp.put(local_file, remote_file)
+        print(f"File '{local_file}' successfully uploaded to '{remote_file}'")
+    else:
+        print(f"Warning: Local file '{local_file}' does not exist.")
+
+
+def get_config_content(server):
+    config_str = f"""[information]
+name={server["hostname"]}
+protocal={server["protocal"]}
+site={server["site"]}
+ip_addr={server["ip_addr"]}
+
+#这里代理默认使用http代理
+[proxy]
+host=127.0.0.1
+port=10809
+
+[spider]
+depth=-1
+time_per_website={server["time_per_website"]}
+# 爬虫连续爬取URL的延时。单位秒
+download_delay = 60
+image_name = aimafan/spider_traffic:latest"""
+
+    return config_str
+
+
+def split_file(input_file, num):
+    # 读取文件内容
+    with open(input_file, "r") as file:
+        lines = file.readlines()
+
+    # 计算每份的大小
+    num_lines = len(lines)
+    chunk_size = (num_lines + num - 1) // num  # 确保平均分配，最后一份可能多几行
+
+    # 分割文件内容并组装字符串
+    parts = []
+    for i in range(num):
+        start_index = i * chunk_size
+        end_index = start_index + chunk_size
+        chunk_lines = lines[start_index:end_index]
+        parts.append("".join(chunk_lines).strip())  # 去掉尾部的换行符
+
+    return parts
+
+
+# 在服务器上异步执行一系列命令
+def handle_server(server):
+    hostname = server["hostname"]
+    username = server["username"]
+    port = server["port"]
+    private_key_path = server["private_key_path"]
+    local_image_path = os.path.join(
+        project_path, "data", "spider_traffic.tar"
+    )  # 本地Docker镜像文件路径
+    remote_image_path = "~/spider_traffic.tar"  # 目标服务器上保存镜像的路径
+    local_xray_path = os.path.join(
+        project_path, "data", "xray_config", server["xray_name"]
+    )
+    remote_xray_path = "~/xray.json"
+    docker_num = int(server["docker_num"])
+    storage_path = server["storage_path"]
+    image_name = config["spider"]["image_name"]
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # try:
+    # 连接服务器,并初始化服务器
+    # 使用私钥连接
+    ssh.connect(hostname, port=port, username=username, key_filename=private_key_path)
+
+    logger.info(f"{hostname}连接成功")
+    # 使用SCP传送Docker镜像文件
+    with SCPClient(ssh.get_transport()) as scp:
+        scp.put(local_image_path, remote_image_path)
+        scp.put(local_xray_path, remote_xray_path)
+
+    # 执行命令
+    server_commands = [
+        "ethtool -K docker0 tso off gso off gro off",
+        f"docker load -i {remote_image_path}",
+    ]
+    main_commands = []
+    base_name = "spider_traffic"
+    url_parts = split_file(os.path.join(project_path, "urls.txt"), docker_num)
+    for i in range(docker_num):
+        container_name = "_".join([base_name, str(i)])
+
+        config_dir = os.path.join(storage_path, container_name, "config")
+        server_commands.append(f"mkdir -p {config_dir}")
+
+        config_content = get_config_content(server)
+        server_commands.append(
+            f"echo '{config_content}' > {os.path.join(config_dir, 'config.ini')}"
+        )
+        server_commands.append(
+            f"echo '{url_parts[i]}' > {os.path.join(config_dir, 'current_docker_url_list.txt')}"
+        )
+        server_commands.append(
+            f"echo '{get_exclude_content()}' > {os.path.join(config_dir, 'exclude_keywords')}"
+        )
+        server_commands.append(f"docker stop {container_name}")
+        server_commands.append(f"docker rm {container_name}")
+
+        server_commands.append(
+            f"docker run -v {os.path.join(storage_path, container_name, 'data')}:/app/data "
+            f"-v {config_dir}:/app/config "
+            f"-v {os.path.join(storage_path, container_name, 'logs')}:/app/logs "
+            f"--privileged -itd --name {container_name} {image_name} /bin/bash"
+        )
+        server_commands.append(
+            f"docker exec  {container_name} ethtool -K eth0 tso off gso off gro off lro off"
+        )
+        server_commands.append(
+            f"docker cp {remote_xray_path} {container_name}:/app/config"
+        )
+        main_commands.append(
+            f"nohup docker exec {container_name} bash action.sh > {os.path.join(storage_path, container_name+'.log')} 2>&1 &"
+        )
+
+    for sever_command in server_commands:
+        exec_command(ssh, sever_command)
+
+    for main_command in main_commands:
+        exec_command(ssh, main_command)
+
+
+# 主函数：并行处理所有服务器
+def main():
+    # 启动线程
+    for server in servers_info:
+        logger.info(f"开始处理{server["hostname"]}")
+        handle_server(server)
+
+
+if __name__ == "__main__":
+    # 运行主程序
+    main()
