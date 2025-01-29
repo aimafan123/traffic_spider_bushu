4a1f9210b978f9e2c0e3f0cc5b1453874734c700
ZGC-BUPT-aimafan <chongrufan@nuaa.edu.cn>
2025-01-29 15:11:03 +0800

    feat: 更新 Traffic Spider 部署工具，增强功能特性    - 适配 tor 配置文件管理，支持 `torrc_s` 目录下的 `torrc_name` 文件。    - 统一 Xray 和 Tor 配置文件的远程传输逻辑。    - 添加 `rmi_images` 方法，支持远程删除 Docker 镜像。    - 更新 CLI 参数，新增 `rmi` 操作，允许远程删除指定镜像。

--- README.md
+++ README.md
@@ -1,28 +1,29 @@
 # 自动部署流量采集程序
 
-这个项目可以实现流量采集程序的自动部署，支持一次性在多个服务器上启动多个docker采集流量。
+Traffic Spider 部署工具是一个 Python 脚本，旨在远程部署和管理多个服务器上的网络爬虫容器。本工具支持通过 SSH 连接到远程服务器，执行命令、上传文件、管理 Docker 容器，并根据配置自动启动爬虫任务。
 
 docker代码项目路径 [spider_traffic.git](https://github.com/ZGC-BUPT-aimafan/spider_traffic.git)
 
-## 功能
+## 功能特性
 
-爬虫包括三种模式（目前还不支持Tor），可以选择每个网站爬取的最大网页数，每个网页最大滚动次数
-
-- [x] direct
-- [x] xray
-- [ ] tor
-
+远程执行 SSH 命令：通过 paramiko 连接服务器并执行命令。
+文件传输：使用 scp 进行远程文件上传。
+Docker 容器管理：支持启动、停止、删除 Docker 容器。
+代理模式：支持 xray、tor、direct 三种代理模式。
+任务分配：支持 URL 文件拆分，将任务均匀分配给多个容器。
 
 ## 部署步骤
 
 0. 前期准备
 - 代码在 `python3.12.3` 下测试通过
-- 需要事先使用spider_traffic.git项目编译docker镜像，并将docker镜像文件放在所有远程服务器的 `~` 路径，并将路径名写入到config.ini中，默认所有远程服务器的镜像路径相同
+- 需要事先使用 `spider_traffic` 项目编译docker镜像，并将docker镜像文件放在所有远程服务器的 `~` 路径，并将路径名写入到config.ini中，默认所有远程服务器的镜像路径相同
 - 需事先在服务器上安装docker并配置公私钥
 - 如果是非root用户登录服务器，需要手动在服务器上执行
 ```
 ethtool -K docker0 tso off gso off gro off
 ```
+
+- 提前删除重名的docker镜像，可以在 `src` 路径使用 `python -m traffic_spider_bushu.action rmi` 指令
 
 1. 安装依赖库
 ```
@@ -31,42 +32,25 @@ pip install -r requirements.txt
 2. 参照 `data/xray_config/trojan.json.example` ，将xray客户端配置文件放到`data/xray_config/`目录中
 3. 参照 `urls.txt.example` ，将需要采集的网站写入到项目目录的`urls.txt`文件中，注意，每行一个付费域
 4. 修改`config/config.ini.example`，并将名称修改为 `config/config.ini`
-5. 将服务器信息填到`src/traffic_spider_bushu/server_info.py.example`，并将文件名称改为`server_info.py`
+5. 将服务器信息填到`src/traffic_spider_bushu/server_info.py.example`，并按照注释填写爬虫具体情况，将文件名称改为`server_info.py`
 6. 在`src`目录，执行
 ```
-python -m traffic_spider_bushu.action
+python -m traffic_spider_bushu.action bushu
 ```
 程序自动执行，部署结束
 
+## 服务器管理
+本工具提供以下管理功能：
 
-## 服务器部署指令
+- 启动部署
+- 停止容器
+- 删除容器
+- 查看服务器信息
+- 删除 Docker 镜像
 
-部署过程将在服务器上依次执行以下指令，如果不需要某些指令，可手动在代码中注释
+具体可以在 `src` 路径下运行 `python -m traffic_spider_bushu.action --help` 查看
 
-```bash
-scp -r data/xray_config/{xray_name} {username}@{host_name}:~/
 
-ethtool -K docker0 tso off gso off gro off  #如果非root用户，该指令不会成功
-
-docker load -i {remote_image_path}
-
-mkdir -p {storage_path}/{container_name}/config # container_name 定义为spider_traffic_i，i是docker序号，从0开始
-
-echo {config_content} > {config_dir}/config.ini # 写入配置文件，配置文件的内容在代码中定义
-echo {url_parts[i]} > {config_dir}/current_docker_url_list.txt  # 将要访问的url写入文件，该docker要访问的url为全部url的1/n份，其中n为开启的docker的数量
-echo {get_exclude_content()} > {config_dir}/exclude_keywords    # 将要排除的内容写入文件，排除的内容在代码中定义
-
-docker stop {container_name}
-docker rm {container_name}
-
-docker run -v {storage_path}/{container_name}/data:/app/data -v {config_dir}:/app/config -v {storage_path}/{container_name}/logs:/app/logs --privileged -itd --name {container_name} {image_name} /bin/bash
-
-docker exec  {container_name} ethtool -K eth0 tso off gso off gro off lro off   # docker内关闭合并包
-
-docker cp {remote_xray_path} {container_name}:/app/config
-
-nohup docker exec {container_name} bash action.sh > {storage_path}/{container_name+'.log'} 2>&1 &   # 后台执行采集指令，并输出日志
-```
 
 ## 服务器监控程序
 可以运行 `bash bin/server_monitoring.sh` 检测服务器运行
--- src/traffic_spider_bushu/action.py
+++ src/traffic_spider_bushu/action.py
@@ -74,10 +74,10 @@ protocal={server["protocal"]}
 site={server["site"]}
 ip_addr={server["ip_addr"]}
 
-#这里代理默认使用http代理
+#这里代理xray使用http代理，tor使用socks5代理
 [proxy]
 host=127.0.0.1
-port=10809
+port={server["proxy_port"]}
 
 [spider]
 depth=-1
@@ -121,6 +121,11 @@ def handle_server(server):
     Args:
         server (dict): 包含服务器信息的字典，如主机名、用户名、端口、私钥路径等。
     """
+    # 爬虫类型只有直连，tor和xray
+    spider_modes = ["direct", "xray", "tor"]
+    if server["spider_mode"] not in spider_modes:
+        raise ValueError("spider_mode must be one of %r." % spider_modes)
+
     # 从服务器信息字典中提取必要的参数
     hostname = server["hostname"]
     username = server["username"]
@@ -130,16 +135,19 @@ def handle_server(server):
     # 远程服务器上保存Docker镜像的路径
     remote_image_path = config["spider"]["remote_image_path"]
 
-    # 本地Xray配置文件的路径
-    local_xray_path = os.path.join(
-        project_path, "data", "xray_config", server["xray_name"]
-    )
+    if server["spider_mode"] == "xray" or server["spider_mode"] == "direct":
+        local_config_path = os.path.join(
+            project_path, "data", "xray_config", server["xray_name"]
+        )
+        remote_config_path = "~/xray.json"
+    elif server["spider_mode"] == "tor":
+        local_config_path = os.path.join(
+            project_path, "data", "torrc_s", server["torrc_name"]
+        )
+        remote_config_path = "~/torrc"
 
     # 本地删除旧pcap文件的脚本路径
     del_old_pcap_path = os.path.join(project_path, "data", "del_old_pcap.sh")
-
-    # 远程服务器上保存Xray配置文件的路径
-    remote_xray_path = "~/xray.json"
 
     # 从服务器信息中获取Docker容器的数量
     docker_num = int(server["docker_num"])
@@ -164,8 +172,8 @@ def handle_server(server):
     exec_command(ssh, f"mkdir {storage_path}")
 
     with SCPClient(ssh.get_transport()) as scp:
-        # 传输Xray配置文件
-        scp.put(local_xray_path, remote_xray_path)
+        # 传输Xray配置文件或torrc文件
+        scp.put(local_config_path, remote_config_path)
         # 传输删除旧pcap文件的脚本
         scp.put(del_old_pcap_path, os.path.join(storage_path, "del_old_pcap.sh"))
 
@@ -233,7 +241,7 @@ def handle_server(server):
 
         # 将Xray配置文件复制到容器内
         server_commands.append(
-            f"docker cp {remote_xray_path} {container_name}:/app/config"
+            f"docker cp {remote_config_path} {container_name}:/app/config"
         )
 
         # 在容器内执行主命令，并将输出重定向到日志文件
@@ -324,6 +332,30 @@ def list_infomation(server):
         print(f"{translations[key]}：{value}")
 
 
+def rmi_images(server):
+    # 暂停服务器中docker运行
+    base_name = "spider_traffic"
+    hostname = server["hostname"]
+    username = server["username"]
+    port = server["port"]
+    private_key_path = server["private_key_path"]
+    image_name = "192.168.194.63:5000/aimafan_spider_traffic:250127_ubuntu24"
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # try:
+    # 连接服务器,并初始化服务器
+    # 使用私钥连接
+    ssh.connect(hostname, port=port, username=username, key_filename=private_key_path)
+
+    logger.info(f"{hostname}连接成功")
+    server_commands = []
+
+    server_commands.append(f"docker rmi {image_name}")
+    for sever_command in server_commands:
+        exec_command(ssh, sever_command)
+
+
 # 主函数：并行处理所有服务器
 def main(action):
     # 启动线程
@@ -338,14 +370,16 @@ def main(action):
             del_docker(server)
         elif action == "list":
             list_infomation(server)
+        elif action == "rmi":
+            rmi_images(server)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="服务器处理脚本")
     parser.add_argument(
         "action",
-        choices=["bushu", "stop", "del", "list"],
-        help="bushu（服务器部署），stop（暂停所有docker），del（删除所有docker和存储数据）,list（列出所有操作的服务器信息）",
+        choices=["bushu", "stop", "del", "list", "rmi"],
+        help="bushu（服务器部署），stop（暂停所有docker），del（删除所有docker和存储数据）,list（列出所有操作的服务器信息）, rmi（删除制定的镜像）",
     )
     args = parser.parse_args()
 
